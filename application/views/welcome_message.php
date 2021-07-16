567ae50b95407a193c0f60be72ee5be1ef7b1d01
Kgotso Maduna <kgmaduna01@gmail.com>
2021-07-16 16:49:18 +0200

    Portfolio website

---
+++ application/views/welcome_message.php
@@ -0,0 +1,381 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Kgotso Maduna Portfolio</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Satisfy" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Laura - v2.2.1
+  * Template URL: https://bootstrapmade.com/laura-free-creative-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top  d-flex justify-content-center align-items-center header-transparent">
+
+    <nav class="nav-menu d-none d-lg-block">
+      <ul>
+        <li class="active"><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#resume">Resume</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+
+      </ul>
+    </nav><!-- .nav-menu -->
+
+  </header><!-- End Header -->
+ 
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <h1>Kgotso Maduna</h1>
+      <h2>I'm a Software Developer</h2>
+      <a href="#about" class="btn-scroll scrollto" title="Scroll Down"><i class="bx bx-chevron-down"></i></a>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+   
+    <!-- ======= About Me Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <span>About Me</span>
+          <h2>About Me</h2>
+         
+        </div>
+
+        <div class="row">
+          <div class="image col-lg-4 d-flex align-items-stretch justify-content-center justify-content-lg-start"></div>
+          <div class="col-lg-8 d-flex flex-column align-items-stretch">
+            <div class="content pl-lg-4 d-flex flex-column justify-content-center">
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul>
+                    <li><i class="icofont-rounded-right"></i> <strong>Name:</strong> Kgotso Maduna</li>
+                    <li><i class="icofont-rounded-right"></i> <strong>Phone:</strong> +27 72 880 8927</li>
+                    <li><i class="icofont-rounded-right"></i> <strong>City:</strong> City : Johannesburg, South Africa</li>
+                  </ul>
+                </div>
+                <div class="col-lg-6">
+                  <ul>
+                    <li><i class="icofont-rounded-right"></i> <strong>Age:</strong> 24</li>
+                    <li><i class="icofont-rounded-right"></i> <strong>Email:</strong> kgmaduna01@gmail.com</li>
+                    <li><i class="icofont-rounded-right"></i> <strong>Freelance:</strong> Available</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="row mt-n4">
+                <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                  <div class="count-box">
+                    <i class="icofont-clock-time" style="color: #2cbdee;"></i>
+                    <span data-toggle="counter-up">1</span>
+                    <p><strong>Years of experience</strong> I am new in the industry and so far I am moving towards a year of experience</p>
+                  </div>
+                </div>
+
+                <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                  <div class="count-box">
+                    <i class="icofont-document-folder" style="color: #8a1ac2;"></i>
+                    <span data-toggle="counter-up">2</span>
+                    <p><strong>Projects</strong> I have worked on 2 projects and excited to say that I am now working on other projects that I will soon mention.</p>
+                  </div>
+                </div>
+
+                <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                 
+                </div>
+
+                <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                  <div class="count-box">
+                    <i class="icofont-award" style="color: #ff2781;"></i>
+                    <span data-toggle="counter-up">2</span>
+                    <p><strong>Awards</strong> Awarded best student in class in 2017, and later on became second in the top 10 best performing students at Tshwane University of Technology</p>
+                  </div>
+                </div>
+              </div>
+            </div><!-- End .content-->
+
+            <div class="skills-content pl-lg-4">
+              <div class="progress">
+                <span class="skill">HTML <i class="val">90%</i></span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+
+              <div class="progress">
+                <span class="skill">CSS <i class="val">80%</i></span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+
+              <div class="progress">
+                <span class="skill">JavaScript <i class="val">75%</i></span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Me Section -->
+
+    <!-- ======= My Resume Section ======= -->
+    <section id="resume" class="resume">
+      <div class="container">
+
+        <div class="section-title">
+          <span>My Resume</span>
+          <h2>My Resume</h2>
+         
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <h3 class="resume-title">Summary</h3>
+            <div class="resume-item pb-0">
+              <h4>Kgotso Maduna</h4>
+              <p><em>Innovative and deadline-driven Software Developer with 1 year of experience designing and developing websites and mobile applications.</em></p>
+              <p>
+              <ul>
+                <li>924 Jabu Mdunge Drive,Tembisa, 1632</li>
+                <li>(+27) 72 880 8927</li>
+                <li>kgmaduna01@gmail.com</li>
+              </ul>
+              </p>
+            </div>
+
+            <h3 class="resume-title">Education</h3>
+            <div class="resume-item">
+              <h4>National diploma in Information Technology(Extended) &amp; Software Development</h4>
+              <h5>2017 - 2021</h5>
+              <p><em>Thswane University of Technology, Pretoria, South Africa</em></p>
+              <p>I have learnt how to develop software, perform systems analysis and design, and also perform database design including Project Management  build </p>
+            </div>
+            <div class="resume-item">
+              <h4>National Senior Certificate &amp; Matric</h4>
+              <h5>2015</h5>
+              <p><em>JB Matabane Secondary, Midrand, South Africa</em></p>
+              <p>I chose the Science stream. Subjects include:</p>
+                 <li>Mathematics,</li>
+                 <li>Physical Sciences,</li>
+                 <li>Life Sciences,</li>
+                 <li> Geography,</li>
+                 <li>Sepedi,</li> 
+                 <li>English.</li>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <h3 class="resume-title">Professional Experience</h3>
+            <div class="resume-item">
+              <h4>Intern Software Developer at Oktiq-IOT</h4>
+              <h5>2021 March - Present</h5>
+              <p><em>02 Mellis Road,Cornerstone House,Mellis Court, Rivonia, Sandton,South Africa</em></p>
+              
+              <ul>
+                <li>Soft and Technical Skills:Project Management, advanced system, network design, technical reports.</li>
+                <li>Databases: Client Server based environment, Software Defined networking. </li>
+                <li> Emerging Technologies: Cloud Computing, Mobile Computing, Mobile Computing, Wireless Computing or any other Scientific network application.</li>
+                <li> Networks: Installation, maintenance, support and management.</li>
+                <li> Operating Systems: installation and upgrading.</li>
+                <li> Hardware: Upgrading, installation, repair and support.</li>
+                <li> Socket Programming: write, test and install packages, support.</li>
+              </ul>
+              </p>
+            </div>
+           
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End My Resume Section -->
+
+    <!-- ======= My Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container">
+
+        <div class="section-title">
+          <span>My Services</span>
+          <h2>My Services</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4 class="title"><a href="">Web Development</a></h4>
+              <p class="description">I can develop and build quality webssites</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4 class="title"><a href="">Mobile Development</a></h4>
+              <p class="description">I can build quality applications</p>
+            </div>
+          </div>
+
+          
+
+        </div>
+
+      </div>
+    </section><!-- End My Services Section -->
+
+  
+
+    <!-- ======= Contact Me Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <span>Contact Me</span>
+          <h2>Contact Me</h2>
+          
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6">
+
+            <div class="row">
+              <div class="col-md-12">
+                <div class="info-box">
+                  <i class="bx bx-share-alt"></i>
+                  <h3>Social Profiles</h3>
+                  <div class="social-links">
+              
+                    <a href="https://www.facebook.com/kgotso.maduna101" class="facebook"><i class="icofont-facebook"></i></a>
+                    <a href="https://www.instagram.com/kgotso_maduna/" class="instagram"><i class="icofont-instagram"></i></a>
+                    <a href="https://www.linkedin.com/mwlite/in/kgotso-maduna-24a4641aa" class="linkedin"><i class="icofont-linkedin"></i></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="info-box mt-4">
+                  <i class="bx bx-envelope"></i>
+                  <h3>Email Me</h3>
+                  <p>kgmaduna01@gmail.com</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="info-box mt-4">
+                  <i class="bx bx-phone-call"></i>
+                  <h3>Call Me</h3>
+                  <p>(+27) 72 880 8927</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="form-row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                  <div class="validate"></div>
+                </div>
+                <div class="col-md-6 form-group">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                  <div class="validate"></div>
+                </div>
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                <div class="validate"></div>
+              </div>
+              <div class="form-group">
+                <textarea class="form-control" name="message" rows="6" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                <div class="validate"></div>
+              </div>
+              <div class="mb-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Me Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <h3>Kgotso Maduna</h3>
+      <p>Highly believes in delivering quality work.</p>
+      <div class="social-links">
+      
+        <a href="https://www.facebook.com/kgotso.maduna101" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="https://www.instagram.com/kgotso_maduna/" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="https://www.linkedin.com/mwlite/in/kgotso-maduna-24a4641aa" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+      <div class="copyright">
+        &copy; Copyright <strong><span>Kgotso</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by  Kgotso Maduna
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
